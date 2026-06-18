1c85bf2aa59d9d87011c3a9b500134a1033212ae
Zolenf <olaf.zylowski@onet.pl>
2026-06-18 19:55:46 +0200

    final?

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	// Serve static files from public directory
+	mux.Handle("/", http.FileServer(http.Dir("public")))
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	log.Fatal(http.ListenAndServe(":"+port, mux))
+}
